8ef89e73316b2e7ebe1e860991503fdbc2859d50
Javier Lorenzo <javier.lorenzo@paradoxsolutions.es>
2025-03-23 19:58:14 +0100

    refactor: search orquestation

--- src/search/search-orchestation/search-orchestation.module.ts
+++ src/search/search-orchestation/search-orchestation.module.ts
@@ -1,4 +1,30 @@
 import { Module } from '@nestjs/common';
+import { SearchPluginsService } from '../search-plugins/plugin-module/search-plugins.service';
+import { GoogleSearch } from '../search-plugins/google-books/infraestructure/google.search';
+import { OpenApiBookSearch } from '../search-plugins/open-library/infraestructure/open-api.search';
+import { GoogleBooksModule } from '../search-plugins/google-books/google-books.module';
+import { OpenLibraryModule } from '../search-plugins/open-library/open-library.module';
 
-@Module({})
+@Module({
+  imports: [GoogleBooksModule, OpenLibraryModule, SearchOrchestationModule],
+  providers: [
+    SearchPluginsService,
+    {
+      provide: 'PLUGIN_REGISTRATION',
+      useFactory: (
+        googleSearch: GoogleSearch,
+        openApiBookSearch: OpenApiBookSearch,
+        pluginsService: SearchPluginsService,
+      ) => {
+        pluginsService.registerPlugin(
+          openApiBookSearch.origin(),
+          openApiBookSearch,
+        );
+        pluginsService.registerPlugin(googleSearch.origin(), googleSearch);
+      },
+      inject: [GoogleSearch, OpenApiBookSearch, SearchPluginsService],
+    },
+  ],
+  exports: [SearchPluginsService],
+})
 export class SearchOrchestationModule {}
--- src/search/search/search.module.ts
+++ src/search/search/search.module.ts
@@ -1,35 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SearchController } from './infraestructure/search.controller';
-import { GoogleBooksModule } from '../search-plugins/google-books/google-books.module';
-import { SearchPluginsService } from '../search-plugins/plugin-module/search-plugins.service';
 import { SearchResolver } from './infraestructure/graphql/search.resolver';
-import { GoogleSearch } from '../search-plugins/google-books/infraestructure/google.search';
-import { OpenLibraryModule } from '../search-plugins/open-library/open-library.module';
-import { OpenApiBookSearch } from '../search-plugins/open-library/infraestructure/open-api.search';
+import { SearchOrchestationModule } from '../search-orchestation/search-orchestation.module';
 
 @Module({
-  imports: [GoogleBooksModule, OpenLibraryModule],
+  imports: [SearchOrchestationModule],
   controllers: [SearchController],
-  providers: [
-    SearchPluginsService,
-    SearchResolver,
-
-    {
-      provide: 'PLUGIN_REGISTRATION',
-      useFactory: (
-        googleSearch: GoogleSearch,
-        openApiBookSearch: OpenApiBookSearch,
-        pluginsService: SearchPluginsService,
-      ) => {
-        pluginsService.registerPlugin(
-          openApiBookSearch.origin(),
-          openApiBookSearch,
-        );
-        pluginsService.registerPlugin(googleSearch.origin(), googleSearch);
-      },
-      inject: [GoogleSearch, OpenApiBookSearch, SearchPluginsService],
-    },
-  ],
-  exports: [SearchPluginsService],
+  providers: [SearchResolver],
 })
 export class SearchModule {}
