e1a016f2394083a272df4de67c777c1e35e9c1e2
Javier Lorenzo <javier.lorenzo@paradoxsolutions.es>
2025-03-23 13:01:43 +0100

    fix: search google books plugin

--- src/search/search/search.module.ts
+++ src/search/search/search.module.ts
@@ -3,11 +3,26 @@ import { SearchController } from './infraestructure/search.controller';
 import { GoogleBooksModule } from '../search-plugins/google-books/google-books.module';
 import { SearchPluginsService } from '../search-plugins/plugin-module/search-plugins.service';
 import { SearchResolver } from './infraestructure/graphql/search.resolver';
+import { GoogleSearch } from '../search-plugins/google-books/infraestructure/google.search';
 
 @Module({
   imports: [GoogleBooksModule],
   controllers: [SearchController],
-  providers: [SearchPluginsService, SearchResolver],
+  providers: [
+    SearchPluginsService,
+    SearchResolver,
+
+    {
+      provide: 'PLUGIN_REGISTRATION',
+      useFactory: (
+        googleSearch: GoogleSearch,
+        pluginsService: SearchPluginsService,
+      ) => {
+        pluginsService.registerPlugin('google-books', googleSearch);
+      },
+      inject: [GoogleSearch, SearchPluginsService],
+    },
+  ],
   exports: [SearchPluginsService],
 })
 export class SearchModule {}
